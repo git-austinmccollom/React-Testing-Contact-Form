ea85ed78fd8d7edfcf7e6ccc729797b9b21a435b
git-austinmccollom <austinmccollom@gmail.com>
2020-09-03 17:10:21 -0500

    Made test for firstname

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,31 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm"
 
 test("renders App without crashing", () => {
-  render(<App />);
+  const wrapper = render(<App />);
+  console.log(wrapper.debug())
+});
+
+// const test_input = ( labelText, inputPlaceholderText, valueToInput ) => {
+//   test(`Test label: ${labelText} and it's input exists, can be typed in, and shows error message`, () => {
+//     const wrapper = render(<App />);
+//     const firstNameLabel = wrapper.getByText(`/${labelText}/i`)
+//     expect(firstNameLabel).toBeInTheDocument();
+//     const firstNameInput = wrapper.getByPlaceholderText(`/${inputPlaceholderText}/i`)
+//     expect(firstNameInput).toBeInTheDocument();
+//     fireEvent.change(firstNameInput, { target: { value: valueToInput } })
+//     expect(firstNameInput.value).toBe(valueToInput)
+//   });
+// }
+
+test("First Name is labeled, can be typed in, and error message shows", () => {
+  const wrapper = render(<App />);
+  const firstNameLabel = wrapper.getByText(/first name*/i)
+  expect(firstNameLabel).toBeInTheDocument();
+  const firstNameInput = wrapper.getByPlaceholderText(/edd/i)
+  expect(firstNameInput).toBeInTheDocument();
+  fireEvent.change(firstNameInput, { target: { value: 'Edd' } })
+  expect(firstNameInput.value).toBe('Edd')
 });
